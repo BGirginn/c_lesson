c06302587d3778e7a052a7729223a04204954de3
BGirginn <girginbora30@gmail.com>
2026-02-01 01:45:38 +0300

    update

--- operators.c
+++ operators.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
     int a = 10, b = 3;
 
@@ -19,6 +19,26 @@ int main(void)
 
     int y = 7;
     printf("Negatif alma (-y)     : %d\n", -y);
+
+    printf("Baslangic degeri      : %d\n", a);
+
+    a = 20;
+    printf("a = 20                : %d\n", a);
+
+    a += 5; // a = a + 5
+    printf("a += 5                : %d\n", a);
+
+    a -= 3; // a = a - 3
+    printf("a -= 3                : %d\n", a);
+
+    a *= 2; // a = a * 2
+    printf("a *= 2                : %d\n", a);
+
+    a /= 4; // a = a / 4
+    printf("a /= 4                : %d\n", a);
+
+    a %= 3; // a = a % 3
+    printf("a %%= 3               : %d\n", a);
 
     return 0;
 }

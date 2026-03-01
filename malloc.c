32f3b39a6726804e54de231f26536a9921fe30b2
BGirgin <girginbora30@gmail.com>
2026-03-01 23:54:44 +0300

    mallOC

---
+++ malloc.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int *p;              // adres tutacak değişken henüz bir yere bağlı değil
+    p = malloc(4);       // 4 byte yer istedik sonra da ilk bytesinin adresini p ye atadık
+
+
+
+    if (!p) {            // eğer yer verilmediyse bu döngü ile sistemi kesip hatadan koruduk bir nevi try catch gibi düşün
+        printf("Bellek ayrilamadi\n");
+        return 1;
+    }
+
+    *p = 7;              // ayrılan yere 7 yaz
+
+    printf("%d\n", *p);  // yazdır
+
+    //! EĞER BİR ADRES YAZDIRACAKSAN %p KULLANMAK ZORUNDASIN
+    printf("%p\n", p);   // pointerin kendisinin bulunduğu adres. //! direkt pointerin adresini yazdırmak için başına hiçbir şey koyma
+    printf("%p\n", &p);  // pointerin tuttuğu nesnenin adresi     //! pointerin tuttuğu değişkenin adresini yazdırmak için başına & koy
+
+    free(p);             // belleği geri ver
+
+    return 0;
+}
--- pointer.c
+++ pointer.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+ void degistir(int *x)
+    {
+        *x = 20; // burada bir poınter tanımlandı pointere adres bağlandığında içine 20 yazacak
+    }; //! bunları main in dışında tanımlaman lazım
 
 int main()
 {
 
-    // pointerlar içinde başka bir variable nin adresini tutan bir variabledir.
+    char c = 'A';
+    char *p = &c;
+
+    printf("c'nin adresi: %p\n", &c);
+    printf("p'nin tuttugu adres: %p\n", p);     // c nin adresi oluyor bu aslında pointer e string gibi atanan adres türündeki değer
+    printf("p'nin gosterdigi deger: %c\n", *p); // son noktadaki değer bu adres değil p nin tuttuğu değişkenin karşılığını gösteriyor
+    printf("p'nin kendi adresi: %p\n", &p);     // p kendisi nerede?
+
+        int sayi = 10;        // değişken tanımlama
+        printf("%d\n",sayi); // sayinin ilk hali
+        degistir(&sayi);      // yukarda tanımlanan pointer e adres bağlandı
+        printf("%d\n", sayi); // sayinin adresi pointer a bağlandı ve sonra o adresteki sayı atandı
 
     return 0;
 }
